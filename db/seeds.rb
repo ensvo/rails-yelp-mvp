43987ee4beeb71bf3a48a027d8004291377adc26
Liz Vo <liz_quack@hotmail.com>
2020-11-04 13:28:02 +0900

    all paths work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+10.times do
+	Restaurant.create!(
+		name: Faker::Movies::LordOfTheRings.character,
+		address: Faker::Movies::LordOfTheRings.location,
+		phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+		category: Restaurant::CATEGORIES.sample
+		)
+
+end
